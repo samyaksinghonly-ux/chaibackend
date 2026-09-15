ef8b33255d9d12bf1a67481fd1347595c65397a2
Samyak Singh <samyaksinghonly@gmail.com>
2026-09-15 19:08:05 +0530

    github changes

--- index.js
+++ index.js
@@ -7,6 +7,30 @@ const app = express()
 //install environment file into the index file firstly then we can use the variables in the environment variable.
 require('dotenv').config()
 
+
+const githubData =   {
+    "login": "mojombo",
+    "id": 1,
+    "node_id": "MDQ6VXNlcjE=",
+    "avatar_url": "https://avatars.githubusercontent.com/u/1?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/mojombo",
+    "html_url": "https://github.com/mojombo",
+    "followers_url": "https://api.github.com/users/mojombo/followers",
+    "following_url": "https://api.github.com/users/mojombo/following{/other_user}",
+    "gists_url": "https://api.github.com/users/mojombo/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/mojombo/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/mojombo/subscriptions",
+    "organizations_url": "https://api.github.com/users/mojombo/orgs",
+    "repos_url": "https://api.github.com/users/mojombo/repos",
+    "events_url": "https://api.github.com/users/mojombo/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/mojombo/received_events",
+    "type": "User",
+    "user_view_type": "public",
+    "site_admin": false
+  }
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -22,6 +46,9 @@ app.get('/login', (req, res) => {
 
 app.get('/youtube', (req, res) => {
   res.send('<h2> chai aur code</h2>')
+})
+app.get('/github', (req, res) => {
+  res.json(githubData)
 })
 
 //this helps in listening in the app it is very important , plus it takes a functionality as well.
